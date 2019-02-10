eab71c4fc0c9e706df9d82bbb898defcacdba0af
TERESA NARVAEZ <TERESA NARVAEZ@DESKTOP-255LVHT>
2019-02-10 16:16:19 -0500

    Prueba2 Invertir Matrices

--- src/matrices/Matriz.java
+++ src/matrices/Matriz.java
@@ -66,13 +66,13 @@ public static Matriz InvertirDosMatrices(Matriz a, Matriz b) throws DimensionesI
         int i, j, filasA, columnasA; 
         filasA = a.getDimension().height; 
         columnasA = a.getDimension().width; 
-        Matriz matrizResultante = new Matriz(filasA, columnasA, false);
+        Matriz matrizResultante3 = new Matriz(filasA, columnasA, false);
         for (j = 0; j < filasA; j++) { 
             for (i = 0; i < columnasA; i++) { 
-                matrizResultante.datos[i][j] += a.datos[i][j] * b.datos[i][j]; 
+                matrizResultante3.datos[i][j] += a.datos[i][j] * b.datos[i][j]; 
             } 
         } 
-        return matrizResultante; }
+        return matrizResultante3; }
  @Override
     public String toString(){
         String ret = "";
